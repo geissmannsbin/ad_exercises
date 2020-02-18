1c21f9d582ba213dc0cbc9fa33fa6b91359aadca
Robin Geissmann <robingeissmann@hotmail.com>
2020-02-18 16:56:19 +0100

    init

---
+++ src/main/java/ch/hslu/sw01/Aha.java
@@ -0,0 +1,44 @@
+package ch.hslu.sw01;
+
+import org.apache.logging.log4j.message.Message;
+
+import java.util.logging.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class Aha {
+
+    private static int execCounter;
+    private static final Logger LOG = LogManager.getLogger(Aha.class);
+
+    Aha(final int n) throws InterruptedException {
+        execCounter = 0;
+        task(n);
+        LOG.info("task Executions: " + execCounter);
+    }
+
+    private static void task(final int n) throws InterruptedException {
+        task1(); task1(); task1(); task1();
+        for (int i = 0; i < n; i++){
+            task2(); task2(); task2();
+            for (int j = 0; j < n; j++) {
+                task3(); task3();
+            }
+        }
+    }
+
+    private static void task1() throws InterruptedException {
+        Thread.sleep(5);
+        execCounter++;
+    }
+
+    private static void task2() throws InterruptedException {
+        Thread.sleep(5);
+        execCounter++;
+    }
+
+    private  static void task3() throws InterruptedException {
+        Thread.sleep(5);
+        execCounter++;
+    }
+}
---
+++ src/main/java/ch/hslu/sw01/Main.java
@@ -0,0 +1,7 @@
+package ch.hslu.sw01;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Aha haha = new Aha(16);
+    }
+}
